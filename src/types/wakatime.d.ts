f532c304004546782d6e768dbcc734c23ab8ee05
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-15 14:12:08 +0530

    :label: Add types for my stats

--- src/types/wakatime.d.ts
+++ src/types/wakatime.d.ts
@@ -1,4 +1,10 @@
 declare module "wakatime-client" {
+  export const RANGE: {
+    LAST_7_DAYS: "LAST_7_DAYS";
+    LAST_30_DAYS: "LAST_30_DAYS";
+    LAST_6_MONTHS: "LAST_6_MONTHS";
+    LAST_YEAR: "LAST_YEAR";
+  };
   export class WakaTimeClient {
     constructor(apiKey: string);
     getMySummary(params: {
@@ -17,6 +23,11 @@ declare module "wakatime-client" {
         projects: any[];
         range: any;
       }[];
+    }>;
+    getMyStats(params: {
+      range: string;
+    }): Promise<{
+      data: any;
     }>;
   }
 }
