3a263e807808f097849f9a87b4a338f06cc81f28
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-24 18:47:14 +0530

    :sparkles: Add Clockify daily fetcher

--- src/api/clockify.ts
+++ src/api/clockify.ts
@@ -1,29 +1,81 @@
 import { config, cosmicSync } from "@anandchowdhary/cosmic";
 import axios from "axios";
+import dayjs from "dayjs";
+import { write } from "../common";
+import { join } from "path";
 cosmicSync("life");
 
 const apiKey = config("clockifyApiKey");
 const workspaceId = config("clockifyWorkspaceId");
 const userId = config("clockifyUserId");
 
-const getTimeData = async () => {
-  const { data } = await axios.get(
-    `https://api.clockify.me/api/v1/workspaces/${workspaceId}/user/${userId}/time-entries`,
+type ClockifyResult = {
+  description: string;
+  id: string;
+  userId: string;
+  projectId: string;
+  workspaceId: string;
+  timeInterval: {
+    start: string;
+    end: string;
+    duration: string;
+  };
+}[];
+
+const getTimeData = async (date: Date) => {
+  const {
+    data,
+  }: {
+    data: ClockifyResult;
+  } = await axios.get(
+    `https://api.clockify.me/api/v1/workspaces/${workspaceId}/user/${userId}/time-entries?start=${encodeURIComponent(
+      dayjs(date).toISOString()
+    )}&end=${encodeURIComponent(dayjs(date).toISOString())}`,
     {
-      headers: {
-        "X-Api-Key": apiKey,
-      },
+      headers: { "X-Api-Key": apiKey },
     }
   );
-  console.log(data);
+  const items = data.map((item) => {
+    delete item.description;
+    delete item.userId;
+    delete item.projectId;
+    delete item.workspaceId;
+    delete item.id;
+    return item;
+  });
+  const itemsByDate: { [index: string]: ClockifyResult } = {};
+  for await (const item of items) {
+    const date = dayjs(item.timeInterval.start);
+    const year = date.format("YYYY");
+    const month = date.format("MM");
+    const day = date.format("DD");
+    itemsByDate[`${year}/${month}/${day}`] =
+      itemsByDate[`${year}/${month}/${day}`] ?? [];
+    itemsByDate[`${year}/${month}/${day}`].push(item);
+  }
+  for await (const key of Object.keys(itemsByDate)) {
+    await write(
+      join(".", "data", "clockify", "history", key, "time-entries.json"),
+      JSON.stringify(itemsByDate[key], null, 2)
+    );
+  }
+  console.log("Clockify: Added time tracking data");
 };
-getTimeData();
+
+export const daily = async () => {
+  console.log("Clockify: Starting...");
+  await getTimeData(dayjs().subtract(1, "day").toDate());
+  console.log("Clockify: Added yesterday's data");
+  await getTimeData(dayjs().toDate());
+  console.log("Clockify: Added today's data");
+  await getTimeData(dayjs().add(1, "day").toDate());
+  console.log("Clockify: Added tomorrow's data");
+  console.log("Clockify: Added daily summaries");
+};
 
 export const getUserId = async () => {
   const { data } = await axios.get(`https://api.clockify.me/api/v1/user`, {
-    headers: {
-      "X-Api-Key": apiKey,
-    },
+    headers: { "X-Api-Key": apiKey },
   });
   console.log("User ID", data.id);
 };
