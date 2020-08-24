09c1a60165a61d55ac9b8d836f6d524c275dddbe
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-24 15:27:57 +0530

    :sparkles: Add Last.fm daily

--- src/daily.ts
+++ src/daily.ts
@@ -1,6 +1,7 @@
 import "./common";
 import { daily as spotify } from "./api/spotify";
 import { daily as rescueTime } from "./api/rescue-time";
+import { daily as lastFm } from "./api/last-fm";
 import { daily as pocketCasts } from "./api/pocket-casts";
 import { daily as wakatime } from "./api/wakatime";
 import { daily as goodreads } from "./api/goodreads";
@@ -12,4 +13,5 @@ import { config } from "@anandchowdhary/cosmic";
   if (config("daily").includes("rescueTime")) await rescueTime();
   if (config("daily").includes("pocketCasts")) await pocketCasts();
   if (config("daily").includes("wakatime")) await wakatime();
+  if (config("daily").includes("lastfm")) await lastFm();
 })();
