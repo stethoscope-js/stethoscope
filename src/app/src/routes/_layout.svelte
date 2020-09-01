15d115e470b64c2ecad791c3093f398e6f810bc7
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-01 20:02:16 +0530

    :tada: Init sapper app in src/app

---
+++ src/app/src/routes/_layout.svelte
@@ -0,0 +1,14 @@
+<style>
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
+
+<main>
+  <slot />
+</main>
