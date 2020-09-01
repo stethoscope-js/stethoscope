42f3e072a3c910d146aa753a504b2aebe6644acb
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-01 21:18:04 +0530

    :sparkles: Add Loading component

---
+++ src/app/src/components/Loading.svelte
@@ -0,0 +1,31 @@
+<style>
+  div {
+    min-height: 20rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  svg {
+    width: 75px;
+    height: 75px;
+    display: block;
+    margin: 0 auto;
+    animation: spin 2s linear infinite;
+  }
+  svg path {
+    fill: #3c6382;
+  }
+  @keyframes spin {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
+
+<div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44"><path
+      d="M40.7 20.7h-6.4a1.7 1.7 0 00.2 3.6h6.2a1.8 1.8 0 000-3.6zm-8.5-5.4l4.2-4.2.2-.2a1.8 1.8 0 00-2.5-2.5l-.1.2-4.2 4.1-.2.2a1.7 1.7 0 002.6 2.4zM22.5 2.7c-1 0-1.7.7-1.8 1.6V10.7a1.8 1.8 0 003.6-.1V4.3c-.1-1-.9-1.6-1.8-1.6zm-7.2 10.1L11 8.6l-.2-.2a1.8 1.8 0 00-2.5 2.5l.2.2 4.1 4 .2.3c.7.6 1.7.5 2.4-.1.7-.7.7-1.8 0-2.5zm-4.6 11.4a1.7 1.7 0 00-.1-3.5H4.3a1.8 1.8 0 000 3.6H10.7zm2.1 5.5l-4.2 4.2-.2.2a1.8 1.8 0 002.5 2.5l.2-.2 4-4.1.3-.2a1.8 1.8 0 00-2.6-2.4zm11.4 4.6a1.8 1.8 0 00-3.5.1v6.3a1.8 1.8 0 003.6 0V34.3zm12.2-.4l-4.1-4-.2-.3a1.8 1.8 0 00-2.4 2.6l4.2 4.2.2.2a1.8 1.8 0 002.5-2.5l-.2-.2z" /></svg>
+</div>
--- src/app/src/routes/index.svelte
+++ src/app/src/routes/index.svelte
@@ -1,5 +1,24 @@
+<script>
+  import { onMount } from "svelte";
+  import Loading from "../components/Loading.svelte";
+
+  let data = [];
+  let loading = false;
+
+  onMount(async () => {
+    loading = true;
+    const response = await fetch(
+      "https://api.github.com/repos/AnandChowdhary/life/contents/data"
+    );
+    data = await response.json();
+    loading = false;
+  });
+</script>
+
 <svelte:head>
   <title>Life</title>
 </svelte:head>
 
-<p>Life...</p>
+{#if loading}
+  <Loading />
+{:else}{data}{/if}
