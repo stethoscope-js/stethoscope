67a7ff5679f15d5433116ffa715f364ccb7f363f
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-01 21:47:25 +0530

    :recycle: Use index.svelte for all routes

--- src/app/src/routes/index.svelte
+++ src/app/src/routes/index.svelte
@@ -1,21 +1,48 @@
 <script>
-  import { onMount } from "svelte";
+  import { stores } from "@sapper/app";
+  const { page } = stores();
   import Loading from "../components/Loading.svelte";
 
+  let query = $page.query;
   let data = [];
   let state = "loading";
 
-  onMount(async () => {
+  $: query = $page.query;
+
+  if (typeof window !== "undefined" && typeof document !== "undefined") {
+    page.subscribe(({ query }) => {
+      update(query)
+        .then(() => (state = "success"))
+        .catch(() => (state = "errir"));
+    });
+  }
+
+  async function update(query) {
     try {
+      state = "loading";
       const response = await fetch(
-        "https://api.github.com/repos/AnandChowdhary/life/contents/data"
+        `https://api.github.com/repos/AnandChowdhary/life/contents/data/${
+          query.path || ""
+        }`
       );
       data = await response.json();
+      if (
+        Array.isArray(data) &&
+        data.length &&
+        typeof data[0].name === "string"
+      ) {
+        data = data.sort((a, b) =>
+          a.name.localeCompare(b.name, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        );
+      }
       state = "success";
     } catch (error) {
       state = "error";
     }
-  });
+  }
 </script>
 
 <svelte:head>
@@ -26,12 +53,15 @@
   <Loading />
 {:else if state === 'error'}
   <p class="error">We got an error in fetching this data</p>
-{:else}
+{:else if Array.isArray(data)}
   <div class="big-links">
     {#each data as item}
-      <a href={`/${item.name}/`}>
+      <a
+        href={`?path=${encodeURIComponent(`${query.path || ''}${item.name}/`)}`}>
         <span>{item.name.split('-').join(' ')}</span>
       </a>
     {/each}
   </div>
+{:else}
+  <pre>{window.atob(data.content)}</pre>
 {/if}
