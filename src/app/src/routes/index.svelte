7569bb2150d47d761ca7c2e79a2b0984d302e0dd
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-02 00:38:45 +0530

    :sparkles: Add list of weeks

--- src/app/src/routes/index.svelte
+++ src/app/src/routes/index.svelte
@@ -5,6 +5,9 @@
 
   let query = $page.query;
   let data = [];
+  let options = [];
+  let partType = "";
+  let currentPath = "";
   let state = "loading";
 
   $: query = $page.query;
@@ -13,8 +16,47 @@
     page.subscribe(({ query }) => {
       update(query)
         .then(() => (state = "success"))
-        .catch(() => (state = "errir"));
+        .catch(() => (state = "error"));
+      list(query)
+        .then(() => (state = "success"))
+        .catch(() => (state = "error"));
     });
+  }
+
+  async function list(query) {
+    const path = query.path || "";
+    if (
+      (path.includes("/weekly/") ||
+        path.includes("/monthly/") ||
+        path.includes("/yearly/")) &&
+      (path.endsWith("graph.png") || path.endsWith("summary.json"))
+    ) {
+      const parts = path.split("/");
+      parts.pop();
+      currentPath = parts[5];
+      parts.pop();
+      const newPath = parts.join("/");
+      partType = parts[2];
+      try {
+        state = "loading";
+        const response = await fetch(
+          `https://github-api-proxy.anandchowdhary.vercel.app/api?endpoint=/repos/AnandChowdhary/life/contents/data/${
+            newPath || ""
+          }`
+        );
+        const json = await response.json();
+        if (Array.isArray(json) && json.length) {
+          options = json.sort((a, b) =>
+            a.name.localeCompare(b.name, undefined, {
+              numeric: true,
+              sensitivity: "base",
+            })
+          );
+        }
+      } catch (error) {
+        state = "error";
+      }
+    }
   }
 
   async function update(query) {
@@ -43,7 +85,6 @@
           })
         );
       }
-      state = "success";
     } catch (error) {
       state = "error";
     }
@@ -59,6 +100,16 @@
 {:else if state === 'error'}
   <p class="error">We got an error in fetching this data</p>
 {:else if typeof data === 'string'}
+  <nav class="options">
+    <strong>{partType}</strong>
+    {#each options as item}
+      <a
+        href={`?path=${encodeURIComponent(item.html_url.split('/master/data/')[1] + '/graph.png')}`}
+        class={currentPath === item.name ? 'active' : ''}>
+        <span>{item.name}</span>
+      </a>
+    {/each}
+  </nav>
   <img alt="" src={data} />
 {:else if Array.isArray(data)}
   <div class="big-links">
@@ -69,6 +120,6 @@
       </a>
     {/each}
   </div>
-{:else}
+{:else if data.content}
   <pre>{window.atob(data.content)}</pre>
 {/if}
