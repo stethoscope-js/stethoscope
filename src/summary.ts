1b23dff50909296b7a6b22f85b152182c5178d3c
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-02 13:14:59 +0530

    :heavy_plus_sign: Add recursive-readdir

--- src/summary.ts
+++ src/summary.ts
@@ -1,8 +1,40 @@
 import "./common";
 import { config } from "@anandchowdhary/cosmic";
-
+import { join } from "path";
 import { summary as googleFit } from "./api/google-fit";
+import { readdir, lstatSync } from "fs-extra";
+import { write } from "./common";
+
+const apiSummary = async () => {
+  const dataTypes = (await readdir(join(".", "data"))).filter((i) =>
+    lstatSync(join(".", "data", i)).isDirectory()
+  );
+  for await (const type of dataTypes) {
+    const services = (await readdir(join(".", "data", type))).filter((i) =>
+      lstatSync(join(".", "data", type, i)).isDirectory()
+    );
+    for await (const service of services) {
+      const durations = (
+        await readdir(join(".", "data", type, service))
+      ).filter(
+        (i) =>
+          lstatSync(join(".", "data", type, service, i)).isDirectory() &&
+          i !== "api"
+      );
+      let data: any = { durations: {} };
+      for await (const duration of durations) {
+        data.durations[duration] = data.durations[duration] ?? {};
+        // data.durations[duration].items =
+      }
+      await write(
+        join(".", "data", type, service, "api", "summary.json"),
+        JSON.stringify(data, null, 2)
+      );
+    }
+  }
+};
 
 (async () => {
-  if (config("daily").includes("googleFit")) await googleFit();
+  // if (config("daily").includes("googleFit")) await googleFit();
+  await apiSummary();
 })();
