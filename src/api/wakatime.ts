23f9e8bdbdd67565174e536e769ed03eb4e0fc64
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-04 23:01:15 +0530

    :recycle: Generate Wakatime, RescueTime summary

--- src/api/wakatime.ts
+++ src/api/wakatime.ts
@@ -5,6 +5,7 @@ import PromisePool from "es6-promise-pool";
 import dayjs from "dayjs";
 import week from "dayjs/plugin/weekOfYear";
 import { WakaTimeClient } from "wakatime-client";
+import { pathExists, lstat, readdir, readJson } from "fs-extra";
 dayjs.extend(week);
 cosmicSync("life");
 
@@ -60,4 +61,97 @@ export const legacy = async () => {
   console.log("Done!");
 };
 
-export const summary = async () => {};
+export const summary = async () => {
+  if (
+    (await pathExists(join(".", "data", "wakatime-time-tracking", "daily"))) &&
+    (
+      await lstat(join(".", "data", "wakatime-time-tracking", "daily"))
+    ).isDirectory()
+  ) {
+    const years = (
+      await readdir(join(".", "data", "wakatime-time-tracking", "daily"))
+    ).filter((i) => /^\d+$/.test(i));
+    const yearData: { [index: string]: number } = {};
+    for await (const year of years) {
+      let yearlySum = 0;
+      const monthlyData: { [index: string]: number } = {};
+      [...Array(13).keys()]
+        .slice(1)
+        .forEach((val) => (monthlyData[val.toString()] = 0));
+      const months = (
+        await readdir(
+          join(".", "data", "wakatime-time-tracking", "daily", year)
+        )
+      ).filter((i) => /^\d+$/.test(i));
+      for await (const month of months) {
+        let monthlySum = 0;
+        const dailyData: { [index: string]: number } = {};
+        [...Array(dayjs(`${year}-${month}-10`).daysInMonth()).keys()]
+          .slice(1)
+          .forEach((val) => (dailyData[val.toString()] = 0));
+        const days = (
+          await readdir(
+            join(".", "data", "wakatime-time-tracking", "daily", year, month)
+          )
+        ).filter((i) => /^\d+$/.test(i));
+        for await (const day of days) {
+          let json = await readJson(
+            join(
+              ".",
+              "data",
+              "wakatime-time-tracking",
+              "daily",
+              year,
+              month,
+              day,
+              "daily-summary.json"
+            )
+          );
+          let dailySum = 0;
+          if (Array.isArray(json)) {
+            json.forEach((record: any) => {
+              if (record.grand_total && record.grand_total.total_seconds) {
+                dailySum += record.grand_total.total_seconds;
+              }
+            });
+          }
+          if (dailySum) dailyData[parseInt(day)] = dailySum;
+          monthlySum += dailySum;
+          yearlySum += dailySum;
+        }
+        if (Object.keys(dailyData).length)
+          await write(
+            join(
+              ".",
+              "data",
+              "wakatime-time-tracking",
+              "summary",
+              "days",
+              year,
+              `${month}.json`
+            ),
+            JSON.stringify(dailyData, null, 2)
+          );
+        if (monthlySum) monthlyData[parseInt(month)] = monthlySum;
+      }
+      if (Object.keys(monthlyData).length)
+        await write(
+          join(
+            ".",
+            "data",
+            "wakatime-time-tracking",
+            "summary",
+            "months",
+            `${year}.json`
+          ),
+          JSON.stringify(monthlyData, null, 2)
+        );
+      if (yearlySum) yearData[parseInt(year)] = yearlySum;
+    }
+    if (Object.keys(yearData).length)
+      await write(
+        join(".", "data", "wakatime-time-tracking", "summary", "years.json"),
+        JSON.stringify(yearData, null, 2)
+      );
+  }
+};
