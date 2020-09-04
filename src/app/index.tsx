b88b7e3109e4a5c803768ecfa12538bfbe62828f
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-05 01:17:29 +0530

    :construction: Fetch latest API from GitHub

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,9 +2,29 @@ import React, { FunctionComponent } from "react";
 import { render } from "react-dom";
 import "./styles.scss";
 import { useLocation } from "wouter";
+import { useSearchParam } from "react-use";
 
 const App: FunctionComponent<{}> = () => {
   const [location] = useLocation();
+  const repo = useSearchParam("repo");
+  const api = useSearchParam("api");
+  const latest = useSearchParam("latest");
+
+  if (latest) {
+    console.log("loading latest");
+    fetch(
+      `https://raw.githubusercontent.com/${repo}/master/data/${api}/api.json`
+    )
+      .then((response) => {
+        if (!response.ok) throw new Error();
+        return response.json();
+      })
+      .then((json) => {
+        console.log("got json", json);
+      })
+      .catch((error) => console.log);
+  }
+
   console.log(location);
 
   return (
