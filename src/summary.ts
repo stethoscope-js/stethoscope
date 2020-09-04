52338aa7a6010d94a926e35602586f57e7fe2a6c
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-04 16:36:09 +0530

    :construction: Set up new summary fetcher

--- src/summary.ts
+++ src/summary.ts
@@ -1,75 +1,99 @@
 import "./common";
 import { config } from "@anandchowdhary/cosmic";
 import { join } from "path";
-import { readdir, lstatSync } from "fs-extra";
+import { readdir, lstatSync, lstat, pathExists, readJson } from "fs-extra";
 import { write } from "./common";
 import recursiveReaddir from "recursive-readdir";
 import dayjs from "dayjs";
 
 export const summary = async () => {
-  // Generate health summaries
+  /**
+   * Generate health summaries
+   */
   const healthCategories = await readdir(join(".", "data", "health"));
+  const data: {
+    [index: string]: { [index: string]: { [index: string]: number } };
+  } = {};
+  for await (const category of healthCategories) {
+    // Find all items that have daily
+    if (
+      (await pathExists(join(".", "data", "health", category, "daily"))) &&
+      (
+        await lstat(join(".", "data", "health", category, "daily"))
+      ).isDirectory()
+    ) {
+      const years = (
+        await readdir(join(".", "data", "health", category, "daily"))
+      ).filter((i) => /^\d+$/.test(i));
+      for await (const year of years) {
+        const months = (
+          await readdir(join(".", "data", "health", category, "daily", year))
+        ).filter((i) => /^\d+$/.test(i));
+        for await (const month of months) {
+          const days = (
+            await readdir(
+              join(".", "data", "health", category, "daily", year, month)
+            )
+          ).filter((i) => /^\d+$/.test(i));
+          for await (const day of days) {
+            let json = await readJson(
+              join(
+                ".",
+                "data",
+                "health",
+                category,
+                "daily",
+                year,
+                month,
+                day,
+                "sessions.json"
+              )
+            );
+            let dailySum = 0;
+            if (Array.isArray(json)) {
+              // If it's a Google Fit health record
+              try {
+                json = json
+                  .sort(
+                    (a, b) =>
+                      dayjs(a.startTime).unix() - dayjs(b.startTime).unix() ||
+                      dayjs(a.endTime).unix() - dayjs(b.endTime).unix()
+                  )
+                  .reduce(
+                    (r: Array<{ startTime: string; endTime: string }>, a) => {
+                      const last = r[r.length - 1] || [];
+                      if (
+                        dayjs(last.startTime).unix() <=
+                          dayjs(a.startTime).unix() &&
+                        dayjs(a.startTime).unix() <= dayjs(last.endTime).unix()
+                      ) {
+                        if (
+                          dayjs(last.endTime).unix() < dayjs(a.endTime).unix()
+                        ) {
+                          last.endTime = a.endTime;
+                        }
+                        return r;
+                      }
+                      return r.concat(a);
+                    },
+                    []
+                  );
+              } catch (error) {}
+              json.forEach((record: any) => {
+                if (record.startTime && record.endTime) {
+                  dailySum += dayjs(record.endTime).diff(
+                    record.startTime,
+                    "second"
+                  );
+                }
+              });
+            }
+            console.log(category, year, month, day, dailySum);
+          }
+        }
+      }
+    }
+  }
 };
 
 summary();
-
-// const apiSummary = async () => {
-//   const dataTypes = (await readdir(join(".", "data"))).filter((i) =>
-//     lstatSync(join(".", "data", i)).isDirectory()
-//   );
-//   for await (const type of dataTypes) {
-//     const services = (await readdir(join(".", "data", type))).filter((i) =>
-//       lstatSync(join(".", "data", type, i)).isDirectory()
-//     );
-//     for await (const service of services) {
-//       const durations = (
-//         await readdir(join(".", "data", type, service))
-//       ).filter(
-//         (i) =>
-//           lstatSync(join(".", "data", type, service, i)).isDirectory() &&
-//           i !== "api"
-//       );
-//       for await (const duration of durations) {
-//         let data: any = {};
-//         (await recursiveReaddir(join(".", "data", type, service, duration)))
-//           .sort((a, b) =>
-//             a.localeCompare(b, undefined, {
-//               numeric: true,
-//               sensitivity: "base",
-//             })
-//           )
-//           .filter((item) => !item.endsWith(".DS_Store"))
-//           .forEach((item) => {
-//             const fullPath = item.split(
-//               join(".", "data", type, service, duration)
-//             )[1];
-//             const fileName = fullPath.split("/").pop();
-//             const directory = fullPath
-//               .split(`/${fullPath.split("/").splice(-1, 1).join("/")}`)[0]
-//               .substring(1);
-//             let title = directory;
-//             if (duration === "daily") {
-//               title = dayjs(title).format("MMMM DD, YYYY");
-//             } else if (duration === "monthly") {
-//               title = dayjs(`${title.split("/").join("-")}-10`).format(
-//                 "MMMM YYYY"
-//               );
-//             } else if (duration === "weekly") {
-//               title = `Week ${title.split("/")[1]}, ${title.split("/")[0]}`;
-//             }
-//             if (fileName) {
-//               data[fileName] = data[fileName] ?? [];
-//               data[fileName].push({
-//                 title,
-//                 directory,
-//               });
-//             }
-//           });
-//         await write(
-//           join(".", "data", type, service, "api", `${duration}.json`),
-//           JSON.stringify(data, null, 2)
-//         );
-//       }
-//     }
-//   }
-// };
