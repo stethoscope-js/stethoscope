b9a54b880d92670290e752b777d825a7784c3ab3
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-05 01:34:08 +0530

    :sparkles: Fetch data from GitHub

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,35 +1,53 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { render } from "react-dom";
 import "./styles.scss";
-import { useLocation } from "wouter";
-import { useSearchParam } from "react-use";
+import { useSearchParam, createMemo } from "react-use";
+import { pick } from "dot-object";
 
 const App: FunctionComponent<{}> = () => {
-  const [location] = useLocation();
+  const path = useSearchParam("path");
   const repo = useSearchParam("repo");
   const api = useSearchParam("api");
   const latest = useSearchParam("latest");
 
+  const [graphData, setGraphData] = useState<{ [index: number]: number }>({});
+
+  const getApiData = async (repo: string, api: string, path: string) => {
+    const response = await fetch(
+      `https://raw.githubusercontent.com/${repo}/master/data/${api}/${path}`
+    );
+    if (!response.ok) throw new Error();
+    return response.json();
+  };
+  const useMemoApiData = createMemo(getApiData);
+
+  if (!repo || !api) return <h1>No repo or API</h1>;
+
   if (latest) {
     console.log("loading latest");
-    fetch(
-      `https://raw.githubusercontent.com/${repo}/master/data/${api}/api.json`
-    )
-      .then((response) => {
-        if (!response.ok) throw new Error();
-        return response.json();
-      })
+    useMemoApiData(repo, api, "api.json")
       .then((json) => {
-        console.log("got json", json);
+        const items = pick(latest, json);
+        if (!Array.isArray(items)) throw new Error();
+        window.location.href = `/?repo=${encodeURIComponent(
+          repo
+        )}&api=${encodeURIComponent(api)}&path=${encodeURIComponent(
+          `summary/${latest}/${items[items.length - 1]}`
+        )}`;
       })
-      .catch((error) => console.log);
+      .catch(console.log);
   }
 
-  console.log(location);
+  if (path) {
+    getApiData(repo, api, path).then(setGraphData).catch(console.log);
+  }
+
+  if (!path) return <h1>No path</h1>;
 
   return (
     <div>
-      <h1>App</h1>
+      <h1>{path ?? "No path"}</h1>
+      <p>{JSON.stringify(graphData)}</p>
     </div>
   );
 };
