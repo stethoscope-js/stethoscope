4b9bce9a42b374f3043a038f74069a81cf830688
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-05 00:57:41 +0530

    :tada: Init index.html, index.tsx

---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Application: React.FunctionComponent<{}> = () => <h1>Application</h1>;
+
+render(<Application />, document.getElementById("root"));
